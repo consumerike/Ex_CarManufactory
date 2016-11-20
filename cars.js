c3338a5db939b39cf8cccdfb58fe2a8a1725f170
Ikechi <consumerike@gmail.com>
2016-11-19 20:24:18 -0600

    The garage is stocked.  Output is not a rqmt

--- cars.js
+++ cars.js
@@ -26,11 +26,29 @@ Model.prototype = new Make();
 Legacy.prototype = new Model();
 
 //create a favorite Car
-let Legacy1 = new Model();
+let Legacy1 = new Legacy();
 Legacy1.plateNumber = "1232";
 
 let Legacy2 = new Model();
-let Legacy3 = new Model();
+Legacy2.plateNumber = "abddef";
 
+let Legacy3 = new Model();
+Legacy3.plateNumber = "feghi";
+
+
+let Outback = function () {
+	this.engine = "v6";
+	this.maxSpeed = "safely";	
+	this.drive = "all-wheel";
+};
+
+Outback.prototype = new Model();
+
+let Outback1 = new Outback();
+Outback1.plateNumber = "poorformJack";
+console.log("OUtback", Outback1 );
+
+let inventory= [Outback1.make,  Outback1.model, " ", "license: ", Outback1.plateNumber, " ", "find us @garage to see the entire inventory"];
+document.getElementById('garage').innerHTML = `The garage is stoked with: ${inventory}`;
 
 
